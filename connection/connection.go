6d006371c82669a5c7ef30b0559c90389bcd009e
erwin <ekolen@freeler.nl>
2023-10-12 17:00:00 +0200

    Added time out to the UDP read

--- connection/connection.go
+++ connection/connection.go
@@ -3,7 +3,6 @@ package connection
 import (
 	"bufio"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -13,7 +12,10 @@ import (
 	"github.com/ouwe-knutselaar/wizz/models"
 )
 
-const Port = "38899"
+const (
+	Port        = "38899"
+	ReadTimeOut = 5
+)
 
 func SendUdpMessage(host string, message *models.RequestPayload) (*models.ResponsePayload, error) {
 	var (
@@ -27,35 +29,37 @@ func SendUdpMessage(host string, message *models.RequestPayload) (*models.Respon
 	time.Sleep(time.Second * 1)
 	// doing connection to UDP
 	if remoteAddr, err = net.ResolveUDPAddr("udp", fmt.Sprintf(`%s:%s`, host, Port)); err != nil {
-		err = errors.New(fmt.Sprintf(`Unable resolve udp: %s`, err))
+		err = fmt.Errorf(`Unable resolve udp: %s`, err)
 		return nil, err
 	}
 	if conn, err = net.DialUDP("udp", nil, remoteAddr); err != nil {
-		err = errors.New(fmt.Sprintf(`Unable to dial up to udp: %s`, err))
+		err = fmt.Errorf(`Unable to dial up to udp: %s`, err)
 		return nil, err
 	}
 	defer conn.Close()
 	// marshall payload to json string
 	if payload, err = json.Marshal(message); err != nil {
-		err = errors.New(fmt.Sprintf(`Unable to marshal payload: %s`, err))
+		err = fmt.Errorf(`Unable to marshal payload: %s`, err)
 		return nil, err
 	}
 	payloadString := string(payload)
-	log.Println(fmt.Sprintf(`Payload string: %s`, payloadString))
+	log.Printf(`Payload string: %s`, payloadString)
 	// send payload to bulb
 	if _, err = conn.Write(payload); err != nil {
-		err = errors.New(fmt.Sprintf(`Unable to send message to UDP: %s`, err))
+		err = fmt.Errorf(`Unable to send message to UDP: %s`, err)
 		return nil, err
 	}
 	// read response from bulb
+	conn.SetReadDeadline(time.Now().Add(ReadTimeOut * time.Second))
+
 	if _, err = bufio.NewReader(conn).Read(response); err != nil {
-		err = errors.New(fmt.Sprintf(`Unable to read message from UDP: %s`, err))
+		err = fmt.Errorf(`Unable to read message from UDP: %s`, err)
 		return nil, err
 	}
 	result := []byte(strings.Trim(string(response), "\x00'"))
 	// convert string result to struct again
 	if err = json.Unmarshal(result, responsePayload); err != nil {
-		err = errors.New(fmt.Sprintf(`Unable to unmarshal payload: %s`, err))
+		err = fmt.Errorf(`Unable to unmarshal payload: %s`, err)
 		return nil, err
 	}
 	return responsePayload, nil
