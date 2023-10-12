6bd90adfa23e0bdcd4c8110c573c305418cb9feb
erwin <ekolen@freeler.nl>
2023-10-12 17:04:44 +0200

    Code improvement

--- connection/connection.go
+++ connection/connection.go
@@ -29,38 +29,32 @@ func SendUdpMessage(host string, message *models.RequestPayload) (*models.Respon
 	time.Sleep(time.Second * 1)
 	// doing connection to UDP
 	if remoteAddr, err = net.ResolveUDPAddr("udp", fmt.Sprintf(`%s:%s`, host, Port)); err != nil {
-		err = fmt.Errorf(`Unable resolve udp: %s`, err)
-		return nil, err
+		return nil, fmt.Errorf(`Unable resolve udp: %s`, err)
 	}
 	if conn, err = net.DialUDP("udp", nil, remoteAddr); err != nil {
-		err = fmt.Errorf(`Unable to dial up to udp: %s`, err)
-		return nil, err
+		return nil, fmt.Errorf(`Unable to dial up to udp: %s`, err)
 	}
 	defer conn.Close()
 	// marshall payload to json string
 	if payload, err = json.Marshal(message); err != nil {
-		err = fmt.Errorf(`Unable to marshal payload: %s`, err)
-		return nil, err
+		return nil, fmt.Errorf(`Unable to marshal payload: %s`, err)
 	}
 	payloadString := string(payload)
 	log.Printf(`Payload string: %s`, payloadString)
 	// send payload to bulb
 	if _, err = conn.Write(payload); err != nil {
-		err = fmt.Errorf(`Unable to send message to UDP: %s`, err)
-		return nil, err
+		return nil, fmt.Errorf(`Unable to send message to UDP: %s`, err)
 	}
 	// read response from bulb
 	conn.SetReadDeadline(time.Now().Add(ReadTimeOut * time.Second))
 
 	if _, err = bufio.NewReader(conn).Read(response); err != nil {
-		err = fmt.Errorf(`Unable to read message from UDP: %s`, err)
-		return nil, err
+		return nil, fmt.Errorf(`Unable to read message from UDP: %s`, err)
 	}
 	result := []byte(strings.Trim(string(response), "\x00'"))
 	// convert string result to struct again
 	if err = json.Unmarshal(result, responsePayload); err != nil {
-		err = fmt.Errorf(`Unable to unmarshal payload: %s`, err)
-		return nil, err
+		return nil, fmt.Errorf(`Unable to unmarshal payload: %s`, err)
 	}
 	return responsePayload, nil
 }
